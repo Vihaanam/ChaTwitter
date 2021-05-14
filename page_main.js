191ff32f6edbcab8981f9657ad87bff070133c28
Vihaanam <77443799+Vihaanam@users.noreply.github.com>
2021-05-14 15:02:18 +0530

    Add files via upload

--- page_main.js
+++ page_main.js
@@ -16,10 +16,32 @@ function send() {
     like = 0;
     firebase.database().ref(room_name).push({
         name : user_name,
-        msg : msg,
+        message : msg,
         like : like
     });
     document.getElementById("msg").value = "";
+}
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+         username = message_data["name"];
+         message = message_data["message"];
+         like = message_data["like"];
+         name_tag = "<h4>" + username + "<img class='user_tick' src='tick.png'></h4>";
+         message_tag = "<h4 class='message_h4'>" + message + "</h4>";
+         button_like = "<button class='btn btn-primary' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+         span_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+         row = name_tag + message_tag + button_like + span_tag;
+         document.getElementById("output").innerHTML += row;
+}});});}
+getData();
+function updateLike(message_id) {
+    button_id = message_id;
+    number_of_likes = document.getElementById(button_id).value;
+    updated_likes = Number(number_of_likes) + 1;
+    firebase.database().ref(room_name).child(button_id).update({
+        like : updated_likes
+    });
 }
 function logOut() {
     localStorage.removeItem("username_of_ChaTwitter");
